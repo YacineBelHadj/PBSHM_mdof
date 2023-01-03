71e2dfe7697a523949b4256e49d327f69290cc8f
Yacine Bel-Hadj <yacine.bel-hadj@vub.be>
2023-01-03 11:50:25 +0100

    initial commit

---
+++ PBSHM_mdof/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='In this project, I try to simulate an Mdof sys all of this is done in the context of population based monitoring.',
+    author='Yacine Bel-Hadj',
+    license='MIT',
+)
