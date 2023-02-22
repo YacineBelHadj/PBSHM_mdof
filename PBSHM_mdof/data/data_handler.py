41db951622dac1c2c487e0b49e6b722707626967
Yacine Bel-Hadj <yacine.bel-hadj@vub.be>
2023-02-22 17:42:02 +0100

    feat:Improving transfer function computation

--- PBSHM_mdof/data/data_handler.py
+++ PBSHM_mdof/data/data_handler.py
@@ -1,54 +1,57 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
 import h5py
+import numpy as np
+from typing import Dict, List, Tuple
 from PBSHM_mdof.system.population import Population
-import re
+import h5py
 
-class HDF5Handler:
+class HDF5DataBuilder:
     def __init__(self, file: h5py.File):
         self.file = file
         
-    def save_simulation_data(self,dt,t_end,population:Population, simulation_data: dict,id_simulation:int,latent_value:float):
-        """
-        Save simulation data to the HDF5 file.
-        simulation_data should be a dictionary with the following structure:
-            {
-                'time': time_data,
-                'population': {
-                    'system_1': {
-                        'simulation_1': {
-                            attrs: {temperature: temperature}
-                            
-                            'output': output_data,
-                            'input': input_data
-                        },
-                        'simulation_2': {...}
-                    },
-                    'system_2': {...}
-                }
-            }
-        """
-
-        population_name = '_'.join([population.name,str(population.state),str(population.anomaly_level)])
-        population_ds = self.file.require_group(population_name)
-        population_ds.attrs['dt'] = dt
-        population_ds.attrs['t_end'] = t_end
-
-        population_ds.attrs['state'] = population.state
-        population_ds.attrs['anomaly_level'] = population.anomaly_level
-        for sys_name, sys_data in population.systems_params.items():
-            system = population_ds.require_group(sys_name)
-            system.attrs['mass'] = sys_data['mass']
-            system.attrs['stiffness'] = sys_data['stiffness']
-            system.attrs['damping'] = sys_data['damping']
-            system.attrs['units'] = 'kg, N/m, Ns/m'
+    def save_population(self, population_names):
+        for name in population_names:
+            population_group = self.file.create_group(name)
+            population_group.attrs['units'] = 'kg, N/m, Ns/m'
         
-        for sys_name, sys_data in simulation_data.items():
+        
+    def save_population_params(self, population):
+        for population_name, population_params in population.system_params.items():
+            system = self.file[population_name].create_group(population_name)
+            system.attrs['mass'] = population_params['mass']
+            system.attrs['stiffness'] = population_params['stiffness']
+            system.attrs['damping'] = population_params['damping']
+    
+    def save_experiment_params(self, population_name, system_name, dt, t_end):
+        system = self.file[population_name][system_name]
+        system.attrs['dt'] = dt
+        system.attrs['t_end'] = t_end
+    
+    def save_simulation_data(self, population_name, system_name, simulation_idx,data):
+        system = self.file[population_name][system_name]
+        simulation = system.create_group(simulation_idx)
+        simulation.create_dataset(data, shape=data.shape, dtype=data.dtype, data=data)
             
-            simulation_ds = population_ds[sys_name].create_group(f'simulation_{id_simulation}')
-            simulation_ds.attrs['latent_value'] = latent_value
-            if 'output' not in simulation_ds:
-                simulation_ds.create_dataset('output', shape=sys_data['output'].shape, dtype=sys_data['output'].dtype, data=sys_data['output'])
-            if 'input' not in simulation_ds:
-                simulation_ds.create_dataset('input', shape=sys_data['input'].shape, dtype=sys_data['input'].dtype, data=sys_data['input'])
+    def set_resonance_frequency(self, population_name, resonance_frequency):
+        self.file[population_name].attrs['resonance_frequency'] = resonance_frequency
+    
+    def set_latent_value(self, population_name, latent_value):
+        self.file[population_name].attrs['latent_value'] = 
+        
+class H5pyHDF5SimulationBuilder:
+    def __init__(self, file: h5py.File):
+        self.file = file
+        
+    def save_simulation_data(self, population_name, system_name, simulation_name, dt, t_end, output_data, input_data, latent_value):
+        population_ds = self.file[population_name]
+        simulation_ds = population_ds[system_name].create_group(simulation_name)
+        simulation_ds.attrs['latent_value'] = latent_value
+        simulation_ds.attrs['dt'] = dt
+        simulation_ds.attrs['t_end'] = t_end
+        simulation_ds.create_dataset('output', shape=output_data.shape, dtype=output_data.dtype, data=output_data)
+        simulation_ds.create_dataset('input', shape=input_data.shape, dtype=input_data.dtype, data=input_data)
+    
     def get_data_iterator(self):
         for population_name in self.file:
             population_group = self.file[population_name]
@@ -58,14 +61,14 @@ class HDF5Handler:
                     simulation_group = system_group[simulation_name]
                     if 'output' in simulation_group:
                         res = {'output':simulation_group['output'][:],
-                        'dt':population_group.attrs['dt'], 
-                        'system_name':system_name, 
-                        'state':population_group.attrs['state'], 
-                        'anomaly_severity':population_group.attrs['anomaly_level'],
-                        'latent_value':simulation_group.attrs['latent_value']}
-    
+                               'dt':simulation_group.attrs['dt'],
+                               't_end':simulation_group.attrs['t_end'],
+                               'population_name':population_name,
+                               'system_name':system_name,
+                               'latent_value':simulation_group.attrs['latent_value'],
+                               'resonance_frequency':population_group.attrs['resonance_frequency']}
                         yield res
-
+    
     def get_data(self, population:str='*', system:str='*', simulation:str='*'):
         """
         Get the output data, system name, state, and anomaly_severity for all the simulations in the HDF5 file.
@@ -74,25 +77,124 @@ class HDF5Handler:
                 {'system_name': system_name, 'state': state, 'anomaly_severity': anomaly_severity, 'output': output_data}
         """
         simulation_data = []
-        population_data = self.file[population]
-        for sys_name in population_data:
-            if system != '*' and sys_name != system:
+        for population_name in self.file:
+            if population != '*' and population_name != population:
                 continue
-            system_group = population_data[sys_name]
-            for sim_name in system_group:
-                if simulation != '*' and sim_name != simulation:
+            population_group = self.file[population_name]
+            for system_name in population_group:
+                if system != '*' and system_name != system:
                     continue
-                simulation_group = system_group[sim_name]
-                output_data = simulation_group['output'][:]
-                state = population_data.attrs['state']
-                anomaly_severity = population_data.attrs['anomaly_level']
-                simulation_data.append({'system_name': sys_name, 'state': state, 'anomaly_level': anomaly_severity, 'output': output_data})
-        time = {'dt':population_data.attrs['dt'] , 't_end':population_data.attrs['t_end']}
+                system_group = population_group[system_name]
+                for
 
-        return simulation_data , time
+
+
+
+
+
+
+
+
+#########################" OLD CODE "#########################
+# import h5py
+# from PBSHM_mdof.system.population import Population
+# import re
+
+# class HDF5Handler:
+#     def __init__(self, file: h5py.File):
+#         self.file = file
+        
+#     def save_simulation_data(self,dt,t_end,population:Population, 
+#     simulation_data: dict,id_simulation:int,latent_value:float,resonance_frequency:dict):
+#         """
+#         Save simulation data to the HDF5 file.
+#         simulation_data should be a dictionary with the following structure:
+#             {
+#                 'time': time_data,
+#                 'population': {
+#                     'system_1': {
+#                         'simulation_1': {
+#                             attrs: {temperature: temperature}
+                            
+#                             'output': output_data,
+#                             'input': input_data
+#                         },
+#                         'simulation_2': {...}
+#                     },
+#                     'system_2': {...}
+#                 }
+#             }
+#         """
+
+#         population_name = '_'.join([population.name,str(population.state),str(population.anomaly_level)])
+#         population_ds = self.file.require_group(population_name)
+#         population_ds.attrs['dt'] = dt
+#         population_ds.attrs['t_end'] = t_end
+
+#         population_ds.attrs['state'] = population.state
+#         population_ds.attrs['anomaly_level'] = population.anomaly_level
+#         population_ds.attrs['resonance_frequency'] = list(resonance_frequency.values())
+
+#         for sys_name, sys_data in population.systems_params.items():
+#             system = population_ds.require_group(sys_name)
+#             system.attrs['mass'] = sys_data['mass']
+#             system.attrs['stiffness'] = sys_data['stiffness']
+#             system.attrs['damping'] = sys_data['damping']
+#             system.attrs['units'] = 'kg, N/m, Ns/m'
+        
+#         for sys_name, sys_data in simulation_data.items():
+            
+#             simulation_ds = population_ds[sys_name].create_group(f'simulation_{id_simulation}')
+#             simulation_ds.attrs['latent_value'] = latent_value
+#             if 'output' not in simulation_ds:
+#                 simulation_ds.create_dataset('output', shape=sys_data['output'].shape, dtype=sys_data['output'].dtype, data=sys_data['output'])
+#             if 'input' not in simulation_ds:
+#                 simulation_ds.create_dataset('input', shape=sys_data['input'].shape, dtype=sys_data['input'].dtype, data=sys_data['input'])
+#     def get_data_iterator(self):
+#         for population_name in self.file:
+#             population_group = self.file[population_name]
+#             for system_name in population_group:
+#                 system_group = population_group[system_name]
+#                 for simulation_name in system_group:
+#                     simulation_group = system_group[simulation_name]
+#                     if 'output' in simulation_group:
+#                         res = {'output':simulation_group['output'][:],
+#                         'dt':population_group.attrs['dt'], 
+#                         'system_name':system_name, 
+#                         'state':population_group.attrs['state'], 
+#                         'anomaly_severity':population_group.attrs['anomaly_level'],
+#                         'latent_value':simulation_group.attrs['latent_value'],
+#                         'resonance_frequency':population_group.attrs['resonance_frequency']}
+    
+#                         yield res
+
+#     def get_data(self, population:str='*', system:str='*', simulation:str='*'):
+#         """
+#         Get the output data, system name, state, and anomaly_severity for all the simulations in the HDF5 file.
+#         Returns:
+#             A list of dictionaries, where each dictionary contains the following information for a single simulation:
+#                 {'system_name': system_name, 'state': state, 'anomaly_severity': anomaly_severity, 'output': output_data}
+#         """
+#         simulation_data = []
+#         population_data = self.file[population]
+#         for sys_name in population_data:
+#             if system != '*' and sys_name != system:
+#                 continue
+#             system_group = population_data[sys_name]
+#             for sim_name in system_group:
+#                 if simulation != '*' and sim_name != simulation:
+#                     continue
+#                 simulation_group = system_group[sim_name]
+#                 output_data = simulation_group['output'][:]
+#                 state = population_data.attrs['state']
+#                 anomaly_severity = population_data.attrs['anomaly_level']
+#                 simulation_data.append({'system_name': sys_name, 'state': state, 'anomaly_level': anomaly_severity, 'output': output_data})
+#         time = {'dt':population_data.attrs['dt'] , 't_end':population_data.attrs['t_end']}
+
+#         return simulation_data , time
 
     
 
 
-    def close(self):
-        self.file.close()
+#     def close(self):
+#         self.file.close()
--- PBSHM_mdof/system/mdof_system.py
+++ PBSHM_mdof/system/mdof_system.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from functools import lru_cache
-from scipy.linalg import expm
+from scipy.linalg import expm , eig
 from scipy.signal import lti , lsim
 import numpy as np
 
@@ -12,7 +12,7 @@ pd.set_option('display.width', 1000)
 pd.set_option('display.colheader_justify', 'center')
 pd.set_option('display.precision', 3)
 @dataclass
-class Mdof_system:
+class MdofSystem:
     M: np.ndarray 
     K: np.ndarray
     C: np.ndarray
@@ -65,13 +65,46 @@ class Mdof_system:
                 '{}'.format(name,pd.DataFrame(self.M), pd.DataFrame(self.K), pd.DataFrame(self.C)))
 
     def eigenvalues(self):
-        return np.linalg.eigvals(self.A)
-    def eigenvecs(self):
-        return np.linalg.eig(self.A)
+        return np.linalg.eigvals(self.A)[::2]
 
-    def resonance_freqeuncy(self):
-        return np.abs(self.eigenvalues()/2/np.pi)[::2]
+    def eigenvecs(self):
         
+        return np 
+
+    def resonance_frequency(self):
+        return np.abs(self.eigenvalues()/2/np.pi)[::2]
+
+    def damping_ratio(self):
+        w = self.eigenvalues()
+        omega_n = np.abs(w/2/np.pi)
+        zeta = -np.real(w)/omega_n
+        return zeta
+
+    def rayleigh_damping_coef(self):
+        zeta = self.damping_ratio()
+        zeta = zeta[:2]
+        omega_n = self.resonance_frequency()*2*np.pi
+        omega_n = omega_n[:2]
+        Left = 1/2*np.array([[1/omega_n[0],omega_n[0]],
+                            [1/omega_n[1],omega_n[1]]])
+        Right = np.array([[zeta[0]],[zeta[1]]])
+        coef = np.linalg.solve(Left,Right)
+        return coef
+
+    
+    def update_damping(self, method: str ='rayleigh',inplace: bool = True):
+        if method == 'rayleigh':
+            coef = self.rayleigh_damping_coef()
+            C_rayleigh = coef[0]*self.M + coef[1]*self.K
+            if inplace:
+                self.C = C_rayleigh
+            else:
+                return C_rayleigh
+        else:
+            raise ValueError('Unknown damping method')
+
+
+
     def check_system(self):
         assert is_diagonal(self.M), "M is not diagonal"
         assert check_rank_matrix((self.M, self.K, self.C)), "M or K is not full rank"
@@ -90,6 +123,25 @@ class Mdof_system:
             H_ij.append(np.linalg.inv((M_s2+C_s+K_s)[:,:,s_i])[i,j])
 
         return H_ij
+    
+    def project_modal(self, x):
+        v = self.eigenvecs()
+        return v.conj().T@x@v
+    
+    def modal_matrices(self):
+        v = self.eigenvecs()
+        
+        coef = self.rayleigh_damping_coef()
+        omega_n = self.resonance_frequency()*2*np.pi
+        m_modal = self.project_modal(self.M)
+        k_modal = self.project_modal(self.K)
+        c_modal = self.project_modal(self.C)
+        xi_modal = coef[0]/2*1/omega_n + coef[1]/2*omega_n
+
+        return m_modal,k_modal,c_modal,xi_modal
+          
+        
+        
 
     def simulate_homemade(self, t, u, x0=None):
         nsamples = len(t)
@@ -107,10 +159,65 @@ class Mdof_system:
         sys = lti(self.A, self.B, self.C_o, self.D)
         return lsim(sys, u, t)
 
-    def simulate_white_noise(self,t,location:int = 7):
-        nsamples = len(t)
-        scale = np.random.uniform(0.5,30)
-        amplitude = 10*np.random.uniform(0.5,3)
-        u = np.zeros((nsamples,self.n_dof))
-        u[:,location] = amplitude*np.random.normal(0,scale,(nsamples,))
-        return u, self.simulate_lsim(t=t,u=u)
+    def simulate_white_noise(self,t:np.ndarray,location:int = 7,amplitude:float = 300):
+            nsamples = len(t)
+            u = np.zeros((nsamples,self.n_dof))
+            u[:,location] = np.random.normal(0,amplitude,(nsamples,))
+            return u, self.simulate_lsim(t=t,u=u)
+
+
+import numpy as np
+from scipy import linalg
+
+import numpy as np
+from scipy import linalg
+
+def polyeig(*A):
+    """
+    Solve the polynomial eigenvalue problem:
+        (A0 + e A1 +...+  e**p Ap)x=0 
+
+    Return the eigenvectors [x_i] and eigenvalues [e_i] that are solutions.
+
+    Usage:
+        X,e = polyeig(A0,A1,..,Ap)
+
+    Most common usage, to solve a second order system: (K + C e + M e**2) x =0
+        X,e = polyeig(K,C,M)
+
+
+    """
+    if len(A)<=0:
+        raise Exception('Provide at least one matrix')
+    for Ai in A:
+        if Ai.shape[0] != Ai.shape[1]:
+            raise Exception('Matrices must be square')
+        if Ai.shape != A[0].shape:
+            raise Exception('All matrices must have the same shapes');
+
+    n = A[0].shape[0]
+    l = len(A)-1 
+    # Assemble matrices for generalized problem
+    C = np.block([
+        [np.zeros((n*(l-1),n)), np.eye(n*(l-1))],
+        [-np.column_stack( A[0:-1])]
+        ])
+    D = np.block([
+        [np.eye(n*(l-1)), np.zeros((n*(l-1), n))],
+        [np.zeros((n, n*(l-1))), A[-1]          ]
+        ]);
+    # Solve generalized eigenvalue problem
+    e, X = linalg.eig(C, D);
+    if np.all(np.isreal(e)):
+        e=np.real(e)
+    X=X[:n,:]
+
+    # Sort eigenvalues/vectors
+    I = np.argsort(e)
+    X = X[:,I][:,::2]
+    e = e[I][::2]
+
+    # Scaling each mode by max
+    X 
+
+    return X, e
